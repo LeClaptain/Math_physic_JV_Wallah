00165cd34b403cf2f87514d9c088589ec42e12a0
PC-DE-GHALI\ghali <ghali.mechkour@telecom-st-etienne.fr>
2024-09-10 17:12:34 -0400

    added particule support

---
+++ src/Particle.cpp
@@ -0,0 +1,18 @@
+﻿#include "Particle.h"
+
+#include "ofAppRunner.h"
+#include "ofGraphics.h"
+
+void Particle::update(){
+    
+    double dt = ofGetLastFrameTime();
+    this->position += velocity;
+    velocity += acceleration * dt;
+}
+
+void Particle::draw() const{
+    ofSetColor(ofColor::black);
+    ofDrawCircle(position, radius+2);
+    ofSetColor(color);
+    ofDrawCircle(position, radius);
+}
