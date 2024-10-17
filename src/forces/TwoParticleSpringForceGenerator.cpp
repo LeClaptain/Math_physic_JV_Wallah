608f13db60c7fe0f48aecb3d0e56ed48624b8ca5
cizor-v <oscar.cizeron06@gmail.com>
2024-10-16 20:04:19 -0400

    first attempt at two particle spring force generator, testing needed

---
+++ src/forces/TwoParticleSpringForceGenerator.cpp
@@ -0,0 +1,12 @@
+#include "TwoParticleSpringForceGenerator.h"
+
+void TwoParticleSpringForceGenerator::updateForce(Particle* particle, float duration)
+{
+	vec3 length = p1->getPosition() - p2->getPosition();
+	vec3 force = length.normalize() * ( k * ( l0 - length.magnitude() ) );
+
+	if (particle == p1)
+		particle->addForce(force);
+	else
+		particle->addForce(-force);
+}
---
+++ src/forces/TwoParticleSpringForceGenerator.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "ForceGenerator.h"
+#include "vec3.h"
+
+class TwoParticleSpringForceGenerator : public ForceGenerator
+{
+private:
+	Particle* p1; // first particle
+	Particle* p2; // second particle
+	float k; // stiffness constant of the spring
+	float l0; // initial length of the spring
+
+public:
+	TwoParticleSpringForceGenerator(Particle* particle1, Particle* particle2, float k, float initialLength) { p1 = particle1; p2 = particle2; this->k = k; l0 = initialLength; }
+
+	void updateForce(Particle* particle, float duration) override;
+};
+
