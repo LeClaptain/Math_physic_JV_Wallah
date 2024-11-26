c646f54c7511936d22b0b4789e1b6b387efd9651
OSCAR RAYMOND CIZERON <orcizeron@etu.uqac.ca>
2024-11-26 11:55:00 -0500

    finished box class, added function declarations to octree class

--- src/collisionUtility/OTBox.h
+++ src/collisionUtility/OTBox.h
@@ -2,28 +2,39 @@
 
 #include "../maths/vec3.h"
 
-class OTBox
+namespace octree
 {
-public:
-	OTBox(float x, float y, float z, float w, float h, float d)
+	class OTBox
 	{
-		position = maths::vec3(x, y, z);
-		extent = maths::vec3(w, h, d);
-	}
+	public:
+		OTBox(float x, float y, float z, float w, float h, float d)
+		{
+			position = maths::vec3(x, y, z);
+			extent = maths::vec3(w, h, d);
+		}
 
-	OTBox(maths::vec3 pos, maths::vec3 ext)
-	{
-		position = pos;
-		extent = ext;
-	}
+		OTBox(maths::vec3 pos, maths::vec3 ext)
+		{
+			position = pos;
+			extent = ext;
+		}
 
-	maths::vec3 getPosition() { return position; }
-	maths::vec3 getExtent() { return extent; }
+		OTBox()
+		{
+			position = maths::vec3(0.0f);
+			extent = maths::vec3(1.0f);
+		}
 
-	bool contains(float box);
+		maths::vec3 getPosition() { return position; }
+		maths::vec3 getExtent() { return extent; }
+		maths::vec3 getMaxEdge() { return maths::vec3(position.x() + extent.x(), position.y() + extent.y(), position.z() + extent.z() / 2); }
+		maths::vec3 getMinEdge() { return maths::vec3(position.x() - extent.x(), position.y() - extent.y(), position.z() - extent.z() / 2); }
 
-private:
-	maths::vec3 position;
-	maths::vec3 extent;
-};
+		bool contains(OTBox& other);
+		bool intersects(OTBox& other);
 
+	private:
+		maths::vec3 position;
+		maths::vec3 extent;
+	};
+}
--- src/collisionUtility/Octree.h
+++ src/collisionUtility/Octree.h
@@ -1,6 +1,41 @@
 #pragma once
-class Octree
-{
 
-};
+#include <vector>
+#include <array>
+#include "OTBox.h"
+#include "BoundingVolume.h"
+
+namespace octree
+{
+	class Octree
+	{
+	public:
+		Octree(OTBox& box)
+		{
+			_box = box;
+			_root = new Node;
+		}
+
+		void add(BoundingVolume* value);
+		bool remove(BoundingVolume* value);
+		std::vector<BoundingVolume*> query(OTBox box);
+
+	private:
+		struct Node
+		{
+			std::array<Node*, 4> children;
+			std::vector<BoundingVolume*> values;
+		};
+
+		OTBox _box;
+		Node* _root;
+
+		void split(Node* node, OTBox box);
+		int getOctant(OTBox nodeBox, OTBox valueBox);
+		OTBox getSubBox(OTBox box, int i); // i is the index of the sub-box: 0-7
+		bool isLeaf(Node* node);
+		void removeValue(Node* node, BoundingVolume* value);
+		bool mergeNode(Node* node);
+	};
+}
 
