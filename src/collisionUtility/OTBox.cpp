0676f3997050fb848aa9164f84efd7d8833dadb0
OSCAR RAYMOND CIZERON <orcizeron@etu.uqac.ca>
2024-11-26 11:55:00 -0500

    finished box class, added function declarations to octree class

--- src/collisionUtility/OTBox.cpp
+++ src/collisionUtility/OTBox.cpp
@@ -1,1 +1,24 @@
 #include "OTBox.h"
+
+bool octree::OTBox::contains(OTBox& other)
+{
+    return  getMaxEdge().x() >= other.getMaxEdge().x() &&
+            getMaxEdge().y() >= other.getMaxEdge().y() &&
+            getMaxEdge().z() >= other.getMaxEdge().z() &&
+            getMinEdge().x() <= other.getMaxEdge().x() &&
+            getMinEdge().y() <= other.getMaxEdge().y() &&
+            getMinEdge().z() <= other.getMaxEdge().z();
+}
+
+bool octree::OTBox::intersects(OTBox& other)
+{
+    return 
+        !( 
+            getMaxEdge().x() <= other.getMinEdge().x() ||
+            getMaxEdge().y() <= other.getMinEdge().y() ||
+            getMaxEdge().z() <= other.getMinEdge().z() ||
+            getMinEdge().x() >= other.getMaxEdge().x() ||
+            getMinEdge().y() >= other.getMaxEdge().y() ||
+            getMinEdge().z() >= other.getMaxEdge().z()
+        );
+}
