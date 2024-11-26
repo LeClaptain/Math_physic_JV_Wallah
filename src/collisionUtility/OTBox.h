6a6c9274c7b3e6a4658525f8bd7736241f8789c2
OSCAR RAYMOND CIZERON <orcizeron@etu.uqac.ca>
2024-11-26 18:17:15 -0500

    finished Octree class, tweaked OTBox, UNTESTED

--- src/collisionUtility/OTBox.h
+++ src/collisionUtility/OTBox.h
@@ -19,6 +19,12 @@ namespace octree
 			extent = ext;
 		}
 
+		OTBox(float x, float y, float z, maths::vec3 ext)
+		{
+			position = maths::vec3(x, y, z);
+			extent = ext;
+		}
+
 		OTBox()
 		{
 			position = maths::vec3(0.0f);
@@ -27,8 +33,8 @@ namespace octree
 
 		maths::vec3 getPosition() { return position; }
 		maths::vec3 getExtent() { return extent; }
-		maths::vec3 getMaxEdge() { return maths::vec3(position.x() + extent.x(), position.y() + extent.y(), position.z() + extent.z() / 2); }
-		maths::vec3 getMinEdge() { return maths::vec3(position.x() - extent.x(), position.y() - extent.y(), position.z() - extent.z() / 2); }
+		maths::vec3 getMaxEdge() { return maths::vec3(position.x() + extent.x() / 2.0f, position.y() + extent.y() / 2.0f, position.z() + extent.z() / 2.0f); }
+		maths::vec3 getMinEdge() { return maths::vec3(position.x() - extent.x() / 2.0f, position.y() - extent.y() / 2.0f, position.z() - extent.z() / 2.0f); }
 
 		bool contains(OTBox& other);
 		bool intersects(OTBox& other);
@@ -38,3 +44,4 @@ namespace octree
 		maths::vec3 extent;
 	};
 }
+
--- src/collisionUtility/Octree.h
+++ src/collisionUtility/Octree.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <array>
+#include <algorithm>
 #include "OTBox.h"
 #include "BoundingVolume.h"
 
@@ -16,25 +17,40 @@ namespace octree
 			_root = new Node;
 		}
 
-		void add(BoundingVolume* value);
-		bool remove(BoundingVolume* value);
-		std::vector<BoundingVolume*> query(OTBox box);
+		
 
-	private:
+		void add(BoundingVolume* value);
+		void remove(BoundingVolume* value);
+		std::vector<BoundingVolume*> query(OTBox queryBox); // returns all elements contained within the queryBox
+
+		OTBox getBox() { return _box; }
+
+	private: 
 		struct Node
 		{
 			std::array<Node*, 4> children;
 			std::vector<BoundingVolume*> values;
 		};
 
-		OTBox _box;
-		Node* _root;
+		OTBox _box; // base box of the octree, should encompass the entire area where there could be collisions
+		Node* _root; // base node
+		const int _THRESHOLD = 8; // maximum amount of elements a node can contain
+		const int _MAXDEPTH = 8; // maximum amount of levels the tree can contain: this means there can be at most 4^_MAXDEPTH + 1 nodes in the tree
 
+		bool isLeaf(Node* node)
+		{
+			return node->children.empty();
+		}
+
+		bool remove(Node* node, OTBox box, BoundingVolume* value);
+		void add(Node* node, int depth, OTBox box, BoundingVolume* value);
+		void query(Node* node, OTBox box, OTBox queryBox, std::vector<BoundingVolume*> values);
+
+		OTBox getBoxFromBV(BoundingVolume* bv); // returns a OTBox fitted to the BoundingVolume
 		void split(Node* node, OTBox box);
-		int getOctant(OTBox nodeBox, OTBox valueBox);
-		OTBox getSubBox(OTBox box, int i); // i is the index of the sub-box: 0-7
-		bool isLeaf(Node* node);
-		void removeValue(Node* node, BoundingVolume* value);
+		int getOctant(OTBox nodeBox, OTBox valueBox); // returns the index of the octant the value is in, indices are detailed below
+		OTBox getSubBox(OTBox box, int i); // i is the index of the sub-box: 0 -> top front left, 1 -> top front right, 2 -> bottom front left, 3 -> bottom front right, 4 -> top back left, 5 -> top back right, 6 -> bottom back left, 7 -> bottom back right
+		bool removeValue(Node* node, BoundingVolume* value);
 		bool mergeNode(Node* node);
 	};
 }
